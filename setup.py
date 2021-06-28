7efc9e6b37fef52c73b5d2163a5b351a67b946cd
Vyryn <hey9055@gmail.com>
2021-06-27 22:34:25 -0400

    v1.0.0-alpha.1.1 Added a missing comma to tags

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'Intended Audience :: Developers',
         'Environment :: Console',
         'Operating System :: OS Independent',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.9',
     ]
 )
